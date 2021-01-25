ec5108f9341ace8a6d44d424c8174bba41558ba3
elaredo <65597821+elaredo@users.noreply.github.com>
2021-01-25 14:48:55 +0100

    Update __init__.py

--- rabbit_force/__init__.py
+++ rabbit_force/__init__.py
@@ -2,3 +2,4 @@
 from ._metadata import VERSION as __version__  # noqa: F401
 from ._metadata import AUTHOR as __author__  # noqa: F401
 from ._metadata import AUTHOR_EMAIL as __author_email__  # noqa: F401
+from  dotenv    import load_dotenv
